8af9e33ff92de754f39772490edfcb567a59c37c
Mohamed Kassem <aboelkassem.me@gmail.com>
2025-03-01 12:53:35 +0200

    feat: add brute force of backspace string compare

---
+++ general-problems/49.Backspace String Compare.cs
@@ -0,0 +1,23 @@
+// Link: https://leetcode.com/problems/backspace-string-compare
+
+// Using Brute force
+// Time: O(n)
+// Space: O(1)
+public class Solution {
+    public bool BackspaceCompare(string s, string t) {
+        return getCleanText(s) == getCleanText(t);
+    }
+    
+    private string getCleanText(string s){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.Length; i++){
+            if(s[i] == '#'){
+                if (sb.Length > 0)
+                    sb.Length--; // remove last character
+            }else{
+                sb.Append(s[i]);
+            }
+        }
+        return sb.ToString();
+    }
+}
